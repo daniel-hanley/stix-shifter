e223f0c50fc5ce839b1a7483401f6556bcc8ce7f
jiss <Jiss.James@ibm.com>
2019-01-16 13:57:49 +0000

    fixing macaddress and file hashes

--- stix_shifter/stix_translation/src/modules/splunk/splunk_translator.py
+++ stix_shifter/stix_translation/src/modules/splunk/splunk_translator.py
@@ -1,7 +1,9 @@
 # ..base.base_translator import BaseTranslator
+from stix_shifter.stix_translation.src.json_to_stix.json_to_stix import JSONToStix
 from ..base.base_translator import BaseTranslator
 from .cim_to_stix.cim_to_stix import CIMToStix
 from .stix_to_splunk import StixToSplunk
+from .splunk_utils import hash_type_lookup
 
 from os import path
 
@@ -12,7 +14,6 @@ class Translator(BaseTranslator):
         basepath = path.dirname(__file__)
         filepath = path.abspath(
             path.join(basepath, "json", "to_stix_map.json"))
-       
         self.mapping_filepath = filepath
-        self.result_translator = CIMToStix(filepath)
+        self.result_translator = JSONToStix(filepath, hash_type_lookup)
         self.query_translator = StixToSplunk()
